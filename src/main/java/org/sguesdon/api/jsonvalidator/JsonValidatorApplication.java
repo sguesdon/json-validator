f2a4a57e79fdca8e16bfbb2ba1afbb947522f101
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2021-09-11 00:44:12 +0200

    initialisation du projet

---
+++ src/main/java/org/sguesdon/api/jsonvalidator/JsonValidatorApplication.java
@@ -0,0 +1,13 @@
+package org.sguesdon.api.jsonvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sguesdon/api/jsonvalidator/JsonValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sguesdon.api.jsonvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
